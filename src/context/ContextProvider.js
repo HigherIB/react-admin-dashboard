acefca5522393e31877003035259501829d7629b
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-26 20:29:49 +0000

    navbar clicks

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -11,7 +11,10 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
-
+    const [isClicked, setIsClicked] = useState(initialState);
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]:true });
+    }
 
 
     return (
@@ -19,6 +22,9 @@ export const ContextProvider = ({ children }) => {
             value={{
                 activeMenu,
                 setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
             }}
         >
             { children }
