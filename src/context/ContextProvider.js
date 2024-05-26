582d4aa10a8429b53a0a30563a79fcac7bfd4dc1
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-26 14:01:31 +0000

    starting contexts

---
+++ src/context/ContextProvider.js
@@ -0,0 +1,29 @@
+import React, { Children, createContext, useContext, useState } from "react";
+
+const stateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+
+
+
+    return (
+        <stateContext.Provider
+            value={{
+                activeMenu,
+                setActiveMenu
+            }}
+        >
+            { children }
+        </stateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(stateContext);
