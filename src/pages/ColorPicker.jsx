e3c5682ef17daf83c52654f01d9fc4bbb6007f52
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-27 19:56:12 +0000

    color picker fully functional

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -30,10 +30,11 @@ const ColorPicker = () => {
                         <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
                         <ColorPickerComponent
                             id="inline-picker"
-                            mode=""
+                            mode="Picker"
                             modeSwitcher={false}
                             inline
                             showButtons={false}
+                            change={change}
                         />
                     </div>
                 </div>
