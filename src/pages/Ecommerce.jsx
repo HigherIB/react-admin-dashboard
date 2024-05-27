5c5693f43a75c5e52b39d5f6ec907829d03766fa
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-27 11:28:45 +0000

    revenue setup1

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { BsCurrencyDollar } from "react-icons/bs";
-import { GoPrimitiveDot } from "react-icons/go";
+import { GoDotFill } from "react-icons/go";
 import { Stacked, Pie, Button, SparkLine } from "../components";
 import { earningData, SparklineAreaData, ecomPieChartData } from "../data/dummy";
 import { useStateContext } from "../context/ContextProvider";
