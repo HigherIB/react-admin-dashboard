a2151898063bcf74c10e3b7c55325aa47c54f3f5
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-27 07:21:56 +0000

    hero banner2

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -12,7 +12,7 @@ const Ecommerce = () => {
         <div className="mt-12">
             <div className="flex flex-wrap lg:flex-nowrap justify-center">
                 <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repat bg-cover bg-center">
-
+                        <h1>e c o m m e r ce</h1>
                 </div>
             </div>
         </div>
