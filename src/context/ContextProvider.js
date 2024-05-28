9eb0008c4935d54b7f50996b79bad1c646a181e9
HigherIB <148780517+HigherIB@users.noreply.github.com>
2024-05-28 19:01:50 +0000

    theme settings roundup

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -15,7 +15,7 @@ export const ContextProvider = ({ children }) => {
     const [screenSize, setScreenSize] = useState(undefined);
     const [currentColor, setCurrentColor] = useState('#03C9D7');
     const [currentMode, setCurrentMode] = useState('Light');
-    const [themesettings, setThemeSettings] = useState(false);
+    const [themeSettings, setThemeSettings] = useState(false);
     
     const setMode = (e) => {
         setCurrentMode(e.target.value);
@@ -23,14 +23,14 @@ export const ContextProvider = ({ children }) => {
         localStorage.setItem('themeMode', e.target.value);
     }
 
-    const setColor = (e) => {
-        setCurrentColor(e.target.value);
+    const setColor = (color) => {
+        setCurrentColor(color);
 
-        localStorage.setItem('colorMode', e.target.value);
+        localStorage.setItem('colorMode', color);
     }
 
     const handleClick = (clicked) => {
-        setIsClicked({ ...initialState, [clicked]:true });
+        setIsClicked({ ...initialState, [clicked]: true });
     }
 
     return (
@@ -43,9 +43,13 @@ export const ContextProvider = ({ children }) => {
                 handleClick,
                 screenSize,
                 setScreenSize,
+                initialState,
+                handleClick,
+                setColor,
+                setMode,
                 currentColor, setCurrentColor,
                 currentMode, setCurrentMode,
-                themesettings, setThemeSettings
+                themeSettings, setThemeSettings
             }}
         >
             { children }
